5c30cf46c58d9c78b04098acca5959ba2509ab0b
nhwangbo <nickhwangbo@gmail.com>
2018-11-10 16:08:25 -0800

    Changed lottery and mysterybox to fully implemented classes.

--- events/lottery.py
+++ events/lottery.py
@@ -3,82 +3,59 @@ import utilities as util
 
 
 file_string = 'json/lottery_info.json'
-init_val = 1000
-max_ticket_number = 5000
 
 
+class Lottery:
+	def __init__(self, points):
+		self.points = points
+		self.lottery_dict = util.load_file(file_string)
 
-def get_value() -> int:
-# returns the lottery pot
-	lottery_dict = util.load_file(file_string)
-	return lottery_dict['value']
+		self.init_val = 1000
+		self.max_ticket_number = 5000
 
+	def get_value(self) -> int:
+		return self.lottery_dict['value']
 
-def get_tickets(player: str) -> int:
-# returns the number of tickets owned by a player
-	lottery_dict = util.load_file(file_string)
-	owners_list = list(lottery_dict.values())
-	tickets = 0
-	for x in range(len(owners_list)):
-		if owners_list[x] == player:
-			tickets += 1
-	return tickets
+	def get_tickets(self, player: str) -> int:
+		# returns how many times the user appears in lottery dict values
+		return list(self.lottery_dict.values()).count(player)
 
+	def get_remaining_tickets(self) -> int:
+		return self.max_ticket_number - len(self.lottery_dict) + 1
 
-def get_remaining_tickets() -> int:
-	lottery_dict = util.load_file(file_string)
-	return max_ticket_number - len(lottery_dict) + 1 # len(lottery_dict) while empty is 1 because of value
+	def user_exists_check(self, player: str) -> bool:
+		return player in self.lottery_dict.values()
 
+	def ticket_exists_check(self, number: int) -> bool:
+		return str(number) in self.lottery_dict
 
-def user_exists_check(player: str) -> bool:
-	lottery_dict = util.load_file(file_string)
-	owners_list = lottery_dict.values()
-	return player in owners_list
+	def generate_ticket(self) -> int:
+		ticket = util.rng(1, self.max_ticket_number)
+		while self.ticket_exists_check(ticket):
+			ticket = util.rng(1, self.max_ticket_number)
+		return ticket
 
+	def buy_ticket(self, player: str, qty: int):
+		for _ in range(qty):
+			self.lottery_dict[str(self.generate_ticket())] = player
+		self.lottery_dict['value'] += qty * 5
+		self.save()
 
-def ticket_exists_check(number: int, lottery_dict: dict) -> bool:
-# checks if a ticket exists in our db
-	# lottery_dict = load_file()
-	return str(number) in lottery_dict
+	def draw(self) -> str:
+		winning_ticket = str(util.rng(1, self.max_ticket_number))
+		if winning_ticket in self.lottery_dict:
+			winner = self.lottery_dict[winning_ticket]
+			self.points.change_points(winner, self.lottery_dict['value'], '+')
+			return winner
+		self.clean_tickets()
 
+	def clean_tickets(self):
+		self.lottery_dict = {'value': self.lottery_dict['value']}
+		self.save()
 
-def generate_ticket(lottery_dict: dict) -> int:
-	ticket = util.rng(1, max_ticket_number)
-	while ticket_exists_check(ticket, lottery_dict):
-		ticket = util.rng(1, max_ticket_number)
-	return ticket
+	def cleanup(self):
+		self.lottery_dict = {'value': self.init_val}
+		self.save()
 
-
-def buy_ticket(player: str, qty: int):
-# buys tickets for a player and increases lottery pot
-	lottery_dict = util.load_file(file_string)
-	for x in range(qty):
-		lottery_dict[str(generate_ticket(lottery_dict))] = player
-	lottery_dict['value'] += qty * 5
-	util.write_file(file_string, lottery_dict)
-
-
-def draw() -> str:
-# json stores the tickets as strings instead of ints
-	winning_ticket = str(util.rng(1, max_ticket_number))
-	lottery_dict = util.load_file(file_string)
-	if winning_ticket in lottery_dict:
-		winner = lottery_dict[winning_ticket]
-		points.change_points(winner, lottery_dict['value'], '+')
-		return winner
-	else:
-		clean_tickets()
-
-
-def clean_tickets():
-# clears all the tickets after a draw
-	lottery_dict = util.load_file(file_string)
-	lottery_value = lottery_dict['value']
-	new_dict = {'value': lottery_value}
-	util.write_file(file_string, new_dict)
-
-
-def cleanup():
-# starts a new lottery after a win
-	new_dict = {'value': init_val}
-	util.write_file(file_string, new_dict)
+	def save(self):
+		util.write_file(file_string, self.lottery_dict)
--- points.py
+++ points.py
@@ -6,89 +6,55 @@ import utilities as util
 
 file_string = 'json/points_table.json'
 
-everyone = ['everybody', 'everyone', 'all']
-admins = ['gay_zach', 'hwangbroxd']
+class Points:
+	def __init__(self):
+		self.points_dict = util.load_file(file_string)
 
+	def check_user_exists(self, user: str) -> bool:
+		return user in self.points_dict
 
-def user_exists_check(user: str) -> bool:
-	# checks if a user currently exists in points_table
-	# returns true if they are and false if not
-	points_dict = util.load_file(file_string)
-	return user in points_dict
+	def remove_user(self, user: str):
+		self.points_dict.pop(user, None)
 
+	def get_points(self, user: str):
+		return self.points_dict[user]
 
-def add_user(user: str):
-	points_dict = util.load_file(file_string)
-	points_dict[user] = 0
-	util.write_file(file_string, points_dict)
+	def get_bot(self) -> list:
+		return sorted(self.points_dict.items(), key=lambda kv: kv[1])
 
+	def update_points(self, usernames = util.get_viewers()):
+		for user in usernames:
+			if user in util.load_blacklist():
+				continue
+			if user not in self.points_dict:
+				self.points_dict[user] = 100
+			elif user in util.admins:
+				self.points_dict[user] += 3
+			else:
+				self.points_dict[user] += 1
 
-def remove_user(user: str):
-	points_dict = util.load_file(file_string)
-	points_dict.pop(user, None)
-	util.write_file(file_string, points_dict)
+	def change_points(self, user: str, points: int, op: str):
+		# op takes in "+", "-", ...
+		ops = {"+": operator.iadd,
+			   "-": operator.isub,
+			   "*": operator.imul,
+			   "/": operator.ifloordiv}
+		update_set = {user}
+		if user in util.everyone:
+			viewers = util.get_viewers()
+			blacklist = util.load_blacklist()
+			update_set.update({v for v in viewers if v not in blacklist})
+		for viewer in update_set:
+			self.points_dict[viewer] = ops[op](self.points_dict[viewer], points)
 
+	def set_points(self, user: str, points: int):
+		update_set = {user}
+		if user in util.everyone:
+			viewers = util.get_viewers()
+			blacklist = util.load_blacklist()
+			update_set.update({v for v in viewers if v not in blacklist})
+		for viewer in update_set:
+			self.points_dict[viewer] = points
 
-def get_points(user: str) -> int:
-	points_dict = util.load_file(file_string)
-	return points_dict[user]
-
-
-def get_bot() -> list:
-	points_dict = util.load_file(file_string)
-	return sorted(points_dict.items(), key=lambda kv: kv[1])
-
-
-def update_points(usernames: [str]):
-	# reads points_table.json as points_dict
-	# edit points_dict for everyone's points
-	# re-write points_table.json with edited points_dict
-	points_dict = util.load_file(file_string)
-
-		# check if there is a new user in chat, then give everyone points
-	for user in usernames:
-		if user not in points_dict:
-			points_dict[user] = 100
-		elif user in admins:
-			points_dict[user] += 3
-		else:
-			points_dict[user] += 1
-	util.write_file(file_string, points_dict)
-
-
-def change_points(user: str, points: int, op: str):
-	# op takes in "+", "-", ...
-	ops = {"+": operator.iadd,
-           "-": operator.isub,
-           "*": operator.imul,
-           "/": operator.ifloordiv}
-	points_dict = util.load_file(file_string)
-	if user in everyone:
-		usernames = util.get_viewers()
-		blacklist = util.load_file('json/blacklist.json')
-		for x in usernames:
-			if x not in blacklist:
-				points_dict[x] = ops[op](points_dict[x], points)
-	else:
-		points_dict[user] = ops[op](points_dict[user], points)
-	util.write_file(file_string, points_dict)
-
-
-def set_points(user: str, points: int):
-	points_dict = util.load_file(file_string)
-	if user in everyone:
-		usernames = util.get_viewers()
-		blacklist = util.load_file('json/blacklist.json')
-		for x in usernames:
-			if x not in blacklist:
-				points_dict[x] = points
-	else:
-		points_dict[user] = points
-	util.write_file(file_string, points_dict)
-
-
-def donate_points(donator: str, recipient: str, value: int):
-	points_dict = util.load_file(file_string)
-	points_dict[donator] -= value
-	points_dict[recipient] += value
-	util.write_file(file_string, points_dict)
+	def save(self):
+		util.write_file(file_string, self.points_dict)
