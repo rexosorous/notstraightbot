11a2f793793ddc6df07f3aac563cd7f138e24d58
J-Zach Loke <jzloke@gmail.com>
2018-11-21 20:42:08 -0800

    fixed an out of range error when user types !redeem

--- gaybot.py
+++ gaybot.py
@@ -852,9 +852,12 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
             rewards = ['dejavu', '90s', 'gas', 'spacejam', 'countryroads', 'fitnessgram', 'skeleton', 'victoryell']
             if len(args) > 2:
                 self.message('Syntax for that command is: !redeem <reward>. Type \"!redeem help\" for more info')
+            elif len(args) == 1:
+                rewards_list = ', '.join(rewards)
+                    self.message(f'rewards cost 1000 points each. here is a list of all the rewards: {rewards_list}')
             else:
                 arg = args[1]
-                if arg == 'help' or cmd_whole == 'redeem':
+                if arg == 'help':
                     rewards_list = ', '.join(rewards)
                     self.message(f'rewards cost 1000 points each. here is a list of all the rewards: {rewards_list}')
                 else:
